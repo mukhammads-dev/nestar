42527dd97252687aa304f097bea788d35bf8e2bf
MARCO <jmukhammadsodikh@gmail.com>
2026-09-23 17:02:35 +0900

    feat: modify getProperties GraphQL API logic

--- apps/nestar-api/src/libs/dto/member/member.input.ts
+++ apps/nestar-api/src/libs/dto/member/member.input.ts
@@ -66,7 +66,7 @@ export class AgentsInquiry {
     limit: number;
 
     @IsOptional()
-    @IsIn([availableAgentSorts])
+    @IsIn(availableAgentSorts)
     @Field(() => String, { nullable: true })
     sort?: string;
 
@@ -109,7 +109,7 @@ export class MembersInquiry {
     limit: number;
 
     @IsOptional()
-    @IsIn([availableMemberSorts])
+    @IsIn(availableMemberSorts)
     @Field(() => String, { nullable: true })
     sort?: string;
 
